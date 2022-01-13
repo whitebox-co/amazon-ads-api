978850e776972492efc2d3b4f9c0d32985899d6e
Melvin Gaye <melvingaye@Melvins-MacBook-Pro.local>
2022-01-13 14:54:11 -0500

    test: product eligibility verifcation tests added

--- tests/integration/apis/clients/common-eligibility-client.spec.ts
+++ tests/integration/apis/clients/common-eligibility-client.spec.ts
@@ -1,7 +1,14 @@
 import * as env from '../../../environment';
 import amazonApi, { CommonEligibilityClient } from '../../../../src/index';
+import { addInterceptor, removeInterceptor } from '../../../../utils/payload-interceptors';
+import {
+	ProductDetails,
+	ProductEligibilityRequest,
+	ProductEligibilityRequestAdTypeEnum,
+} from '../../../../src/apis/models/common-eligibility';
 
 describe('CommonEligibilityClient', () => {
+	let interceptorId: any;
 	let commonEligibilityClient: CommonEligibilityClient;
 
 	beforeAll(async () => {
@@ -13,7 +20,50 @@ describe('CommonEligibilityClient', () => {
 		});
 	});
 
+	beforeEach(() => {
+		const testName = expect.getState().currentTestName;
+		interceptorId = addInterceptor(commonEligibilityClient, testName);
+	});
+
+	afterEach(() => {
+		removeInterceptor(commonEligibilityClient, interceptorId);
+	});
+
 	it('should instantiate the api', () => {
 		expect(commonEligibilityClient).toBeDefined();
+	});
+
+	describe('#productEligibility', () => {
+		it('should verify a sponsored product eligibility', async () => {
+			const productDetails: ProductDetails = { asin: 'Test asin', sku: 'Test sku' };
+			const product: ProductEligibilityRequest = {
+				adType: ProductEligibilityRequestAdTypeEnum.Sp,
+				productDetailsList: [productDetails],
+			};
+
+			const result = await commonEligibilityClient.productEligibility({
+				amazonAdvertisingAPIClientId: env.AMAZON_ADS_CLIENT_ID,
+				amazonAdvertisingAPIScope: env.AMAZON_ADS_PROFILE_ID,
+				productEligibilityRequest: product,
+			});
+
+			expect(result.data).toBeDefined();
+		});
+
+		it('should verify a sponsored brand eligibility', async () => {
+			const productDetails: ProductDetails = { asin: 'Test asin', sku: 'Test sku' };
+			const product: ProductEligibilityRequest = {
+				adType: ProductEligibilityRequestAdTypeEnum.Sb,
+				productDetailsList: [productDetails],
+			};
+
+			const result = await commonEligibilityClient.productEligibility({
+				amazonAdvertisingAPIClientId: env.AMAZON_ADS_CLIENT_ID,
+				amazonAdvertisingAPIScope: env.AMAZON_ADS_PROFILE_ID,
+				productEligibilityRequest: product,
+			});
+
+			expect(result.data).toBeDefined();
+		});
 	});
 });
